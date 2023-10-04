7838c7b6bef5de7452408b166c4d593d85843095
Yovani Morales <83714102+Yovani-Morales@users.noreply.github.com>
2023-10-04 16:38:53 -0600

    Upload all files

---
+++ app/about/page.jsx
@@ -0,0 +1,10 @@
+function AboutPage() {
+    return (
+      <div>
+        <h1>About</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime amet animi obcaecati ea autem voluptates a soluta ut mollitia quam, consequatur illo culpa ipsam repudiandae, et fuga, quasi dolore dolor tempore repellendus dolorum? Provident doloremque, nihil dolor sint libero eaque quis eligendi voluptatum aliquam, quidem porro? Repudiandae eaque qui, blanditiis ad ducimus nostrum similique necessitatibus quidem rem praesentium sapiente, quaerat, nihil molestias cum quo voluptatibus iure velit temporibus cupiditate. Possimus, quod neque cumque consequatur aperiam similique facilis minus sequi accusamus maxime temporibus quasi in repellat illo ex modi! Laudantium maiores eligendi aliquid! Non provident hic quis iusto vitae eum autem!</p>
+      </div>
+    )
+}
+  
+export default AboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,10 @@
+function ServicesPage() {
+    return (
+        <div>
+            <h1>Services</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis sequi quibusdam similique mollitia, perferendis eveniet. Inventore sit quo dolore, quam temporibus enim quos accusantium veniam pariatur maxime beatae hic ducimus! Consequuntur veniam, voluptates a laborum repellat rem commodi aliquid mollitia nemo veritatis vero voluptatum aut fuga neque omnis praesentium error.</p>
+        </div>
+    )
+}
+
+export default ServicesPage
